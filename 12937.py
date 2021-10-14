f470002231937b21a5764874601ab40b549aa964
nsms556 <nsms556@kakao.com>
2021-10-14 18:12:19 +0900

    add 12937

---
+++ 12937.py
@@ -0,0 +1,11 @@
+# 1014 짝수와 홀수
+# https://programmers.co.kr/learn/courses/30/lessons/12937
+
+def solution(num):
+    return 'Even' if num % 2 == 0 else 'Odd'
+
+num = 3
+print(solution(num))
+
+num = 4
+print(solution(num))
